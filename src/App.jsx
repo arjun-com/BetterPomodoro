924340d920239525156cc61e19ef8d69c3163024
Arjun <rao.d.arjun@gmail.com>
2024-12-12 23:12:15 +0530

    First commit.

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+import Clock from './components/Clock'
+import Sidebar from './components/Sidebar'
+import { ColorProvider } from './contexts/ColorContext'
+
+function App() {
+	return (
+		<ColorProvider>
+			<div className="w-screen h-screen overflow-hidden" style={{
+				background: `linear-gradient(to bottom right, var(--bgColor1, #0f2027), var(--bgColor2, #203a43), var(--bgColor3, #2c5364))`
+			}}>
+				<Sidebar />
+				<Clock visibility={{ phaseText: true, timer: true, controls: true }} />
+			</div>
+		</ColorProvider>
+	)
+}
+
+export default App
